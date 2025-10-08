73aa19567df250adae0e4eb082a2b8f91cc31c90
FelixBedoya15 <wappyinteractivo@gmail.com>
2025-10-08 10:39:10 -0500

    0.6.33

--- src/lib/components/chat/SettingsModal.svelte
+++ src/lib/components/chat/SettingsModal.svelte
@@ -8,6 +8,7 @@
 
 	import Modal from '../common/Modal.svelte';
 	import Account from './Settings/Account.svelte';
+	import About from './Settings/About.svelte';
 	import General from './Settings/General.svelte';
 	import Interface from './Settings/Interface.svelte';
 	import Audio from './Settings/Audio.svelte';
@@ -416,6 +417,7 @@
 				'webhook url',
 				'webhookurl'
 			]
+		}
 		}
 	];
 
@@ -861,7 +863,6 @@
 						saveHandler={() => {
 							toast.success($i18n.t('Settings saved successfully!'));
 						}}
-					/>
 				{/if}
 			</div>
 		</div>
